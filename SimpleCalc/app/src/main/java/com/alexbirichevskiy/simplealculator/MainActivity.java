3d25933a5b9b87f855e441417ced6a4b878ffdac
Alexandr Birichevskiy <alexandrbirichevskiy@gmail.com>
2021-05-03 19:15:53 +0300

    Homework for lesson3 v.1.0

--- SimpleCalc/app/src/main/java/com/alexbirichevskiy/simplealculator/MainActivity.java
+++ SimpleCalc/app/src/main/java/com/alexbirichevskiy/simplealculator/MainActivity.java
@@ -2,12 +2,23 @@ package com.alexbirichevskiy.simplealculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button1).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button1) {
+            Toast.makeText(getApplicationContext(), "1", Toast.LENGTH_SHORT).show();
+        }
     }
 }
