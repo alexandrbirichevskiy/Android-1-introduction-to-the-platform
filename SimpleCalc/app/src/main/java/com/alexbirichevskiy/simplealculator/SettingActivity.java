7f7e7d9e10d4d15aee02a836fe2467725e492742
Alexandr Birichevskiy <alexandrbirichevskiy@gmail.com>
2021-05-22 02:37:41 +0300

    SettingActivity change theme

--- SimpleCalc/app/src/main/java/com/alexbirichevskiy/simplealculator/SettingActivity.java
+++ SimpleCalc/app/src/main/java/com/alexbirichevskiy/simplealculator/SettingActivity.java
@@ -1,45 +1,55 @@
 package com.alexbirichevskiy.simplealculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.res.Resources;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
 
-public class SettingActivity extends AppCompatActivity {
 
-    private int theme;
+public class SettingActivity extends AppCompatActivity {
+    public static final String appTheme = "APP_THEME";
+    public static final String nameSharedPreference = "THEME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(theme);
-//        SettingActivity.this.setTheme(R.style.ActivityBackgroundLight);
+        setTheme(getAppTheme(R.style.ActivityBackgroundLight));
         setContentView(R.layout.activity_setting);
+
         RadioButton radioButtonLight = findViewById(R.id.lightThemeButton);
         RadioButton radioButtonDark = findViewById(R.id.DarkThemeButton);
-        radioButtonLight.setChecked(true);
-//
-        radioButtonLight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                theme = R.style.ActivityBackgroundLight;
-                setTheme(theme);
-                recreate();
-                radioButtonLight.setChecked(true);
-            }
-        });
 
         radioButtonDark.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                theme = R.style.ActivityBackgroundDark;
-//                setTheme(R.style.ActivityBackgroundDark);
-                setTheme(theme);
+                setAppTheme(R.style.ActivityBackgroundDark);
                 recreate();
-                radioButtonDark.setChecked(true);
             }
         });
+
+        radioButtonLight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setAppTheme(R.style.ActivityBackgroundLight);
+                recreate();
+            }
+        });
+    }
+
+    private void setAppTheme(int code){
+        SharedPreferences sharedPreferences = getSharedPreferences(nameSharedPreference, MODE_PRIVATE);
+        sharedPreferences.edit()
+                .putInt(appTheme, code)
+                .apply();
+    }
+
+    private int getAppTheme(int code){
+        return getCodeStyle(code);
+    }
+
+    private int getCodeStyle (int codeStyle){
+        SharedPreferences sharedPreferences = getSharedPreferences(nameSharedPreference, MODE_PRIVATE);
+        return sharedPreferences.getInt(appTheme, codeStyle);
     }
 }
